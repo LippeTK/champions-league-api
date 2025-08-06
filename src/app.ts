ded00863f6f146ac9f3817fd98d29825f3ba4070
Felipe Carvalho <104037790+LippeTK@users.noreply.github.com>
2025-08-06 14:45:37 -0300

    fix: cors

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import express, {json, Request, Response} from "express"
 import router from "./routes/playerRoutes"
 import clubsRouter from "./routes/clubsRoutes"
+import cors from "cors"
 
 function createApp(){
     const app = express()
 
     app.use(json())
+    app.use(cors())
 
     app.use('/', router)
     app.use('/clubs', clubsRouter)
